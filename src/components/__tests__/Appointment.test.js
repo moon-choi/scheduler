99a786c4ddc5bcb4525f8eae0e4c88dc3acb1beb
Moon Choi <choimy1991@gmail.com>
2022-05-26 13:25:41 -0700

    Our Test First

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import Appointment from "components/Appointment/index.js";
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("does something it is supposed to do", () => {
+    // ...
+  });
+
+  it("does something else it is supposed to do", () => {
+    // ...
+  });
+});
