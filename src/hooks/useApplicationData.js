823062aaf72ddf3f4a5ce701f14d957141f9a612
Moon Choi <choimy1991@gmail.com>
2022-05-26 19:16:05 -0700

    remove http://localhost:8001

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -124,9 +124,9 @@ export default function useApplicationData() {
   useEffect(() => {
     // axios.get("/api/days").then(response => setDays(response.data)); //response.data === days.
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ]).then((all) => {
       // console.log('all', all)
       setState(prev => ({
